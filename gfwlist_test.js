6c23ddbf7336617827b7089942b6b8dd07087bca
Min Liu <wzygzlm@gmail.com>
2022-05-10 22:34:31 +0200

    Minor change.

--- gfwlist_test.js
+++ gfwlist_test.js
@@ -2,17 +2,11 @@
 ! Title: GFWList BACK TO CN
 ! GFWList with Unlock CHINA streaming
 ! Chinese secondary domain
-! iqiyi
 ||iqiyi.com
 ||iqiyipic.com
 ||qy.net
-
-! Speed test
 ||speedtest.cn
-
-! IP address test
 ||ip138.com
 ||upyun.com
-
 ||google.com
 ||google.com.hk
