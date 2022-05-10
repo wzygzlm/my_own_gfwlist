76b0f6a823ace7c17c7d46bf17c443db82d63609
Min Liu <wzygzlm@gmail.com>
2022-05-10 22:08:25 +0200

    Added test file.

---
+++ gfwlist_test.js
@@ -0,0 +1,15 @@
+[AutoProxy 0.2.9]
+! Title: GFWList BACK TO CN
+! GFWList with Unlock CHINA streaming
+! Chinese secondary domain
+! iqiyi
+||iqiyi.com
+||iqiyipic.com
+||qy.net
+
+! Speed test
+||speedtest.cn
+
+! IP address test
+||ip138.com
+||upyun.com
